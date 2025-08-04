991183c9e222f251e43891f01eebd06e75be3822
Manal El-bakkouri <156050054+manalelbakkouri@users.noreply.github.com>
2025-08-04 21:26:10 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+
+        res=defaultdict(list) 
+        for i in strs :
+            count=[0]*26
+            for c in i :
+                count[ord(c)-ord("a")]+=1
+            res[tuple(count)].append(i)
+        return res.values()
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        
